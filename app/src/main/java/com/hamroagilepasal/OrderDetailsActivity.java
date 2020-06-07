f40b165460d0c60c19de2c49a935f70d90eb289e
CHOCHHE <shrijan822@gmail.com>
2020-06-07 15:15:15 +0545

    Order Details Layout generated

---
+++ app/src/main/java/com/hamroagilepasal/OrderDetailsActivity.java
@@ -0,0 +1,14 @@
+package com.hamroagilepasal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OrderDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order_details2);
+    }
+}
