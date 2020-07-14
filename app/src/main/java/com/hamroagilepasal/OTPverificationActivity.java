5958129feaa77dcb198ff568e1395ecffd97c9fc
CHOCHHE <shrijan822@gmail.com>
2020-07-14 15:38:34 +0545

    OTP verification layout

---
+++ app/src/main/java/com/hamroagilepasal/OTPverificationActivity.java
@@ -0,0 +1,14 @@
+package com.hamroagilepasal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OTPverificationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_o_t_pverification);
+    }
+}
